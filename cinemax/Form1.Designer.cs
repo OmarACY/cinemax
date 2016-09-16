543ce471367152da43981bc9c10dcd1ccde04043
OmarACY <omydi1225@gmail.com>
2016-09-15 23:29:01 -0600

    Personalizacion del formulario principal
    Se agrego una imagen de fonto y se realizaron cambios de codigo para la
    interaccion del formulario

--- cinemax/Form1.Designer.cs
+++ cinemax/Form1.Designer.cs
@@ -28,20 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tbCerrar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tbCerrar
+            // 
+            this.tbCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(20)))));
+            this.tbCerrar.BackgroundImage = global::cinemax.Properties.Resources.cerrar2;
+            this.tbCerrar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tbCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tbCerrar.Location = new System.Drawing.Point(966, 2);
+            this.tbCerrar.Name = "tbCerrar";
+            this.tbCerrar.Size = new System.Drawing.Size(32, 28);
+            this.tbCerrar.TabIndex = 0;
+            this.tbCerrar.UseVisualStyleBackColor = false;
+            this.tbCerrar.Click += new System.EventHandler(this.tbCerrar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 197);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(20)))));
+            this.BackgroundImage = global::cinemax.Properties.Resources.fondo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.tbCerrar);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(20)))));
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button tbCerrar;
     }
 }
 
--- cinemax/Form1.cs
+++ cinemax/Form1.cs
@@ -12,9 +12,39 @@ namespace cinemax
 {
     public partial class Form1 : Form
     {
+        Point formPosition;
+        bool mouseAction;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        #region Metodos del formulario
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseAction == true)
+            {
+                Location = new Point(Cursor.Position.X - formPosition.X,Cursor.Position.Y - formPosition.Y);
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            formPosition = new Point (Cursor.Position.X - Location.X, Cursor.Position.Y - Location.Y);
+            mouseAction = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseAction = false;
+        }
+
+        private void tbCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+#endregion
+
     }
 }
