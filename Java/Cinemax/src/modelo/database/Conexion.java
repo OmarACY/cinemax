84d8959ac2f24109bbba5548c16a8789ceec8994
alessandro03 <alessandro6293@gmail.com>
2016-10-08 22:22:42 -0500

    Clase Conexión
    Se agrega la clase que se encargará de interactuar con la base de datos.

---
+++ Java/Cinemax/src/modelo/database/Conexion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.database;
+
+import java.sql.*;
+
+/**
+ *
+ * @author MILAN
+ */
+public class Conexion {
+    private Connection db;
+    
+    public Conexion(){
+        db = null;
+    }
+    
+    private void realizaConexion() throws ClassNotFoundException, SQLException {
+        String nombreDriver  ="org.postgresql.Driver";
+        int puerto = 5432;
+        String baseDatos = "Cinemax";
+        String url = String.format("jdbc:postgresql://localhost:%d/%s", puerto, baseDatos);
+        // Usuario y Contraseña MILAN-PC 
+        String nombreUsuario = "root";
+        String contraseña = "toor";
+        
+        Class.forName(nombreDriver);
+        db = DriverManager.getConnection(url, nombreUsuario, contraseña);
+    }
+    
+    private void cierraConexion() throws SQLException{
+        db.close();
+        db = null;
+    }
+}
