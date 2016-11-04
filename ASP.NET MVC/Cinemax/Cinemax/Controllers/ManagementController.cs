b010ac41ca8cc3c9ea7b5c17ea03707a9030b08d
alessandro03 <alessandro6293@gmail.com>
2016-11-04 05:30:55 -0600

    Se agregan las vistas necesarias, tanto administrativas como de ventas

---
+++ ASP.NET MVC/Cinemax/Cinemax/Controllers/ManagementController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinemax.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management/Employees
+        public ActionResult Employees()
+        {
+            return View();
+        }
+
+        // GET: Management/Clients
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        // GET: Management/MovieTeathers
+        public ActionResult MovieTeathers()
+        {
+            return View();
+        }
+
+        // GET: Management/Movies
+        public ActionResult Movies()
+        {
+            return View();
+        }
+
+        // GET: Management/FilmFunctions
+        public ActionResult FilmFunctions()
+        {
+            return View();
+        }
+    }
+}
