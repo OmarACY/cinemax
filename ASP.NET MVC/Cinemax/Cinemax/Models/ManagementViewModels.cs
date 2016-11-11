a96fbe6731e59ccfab257ef82b5bcac0f5095ac7
alessandro03 <alessandro6293@gmail.com>
2016-11-11 11:46:32 -0600

    Implementacion de modelos para las vistas

--- ASP.NET MVC/Cinemax/Cinemax/Models/ManagementViewModels.cs
+++ ASP.NET MVC/Cinemax/Cinemax/Models/ManagementViewModels.cs
@@ -83,33 +83,176 @@ namespace Cinemax.Models
 
     public class ClientsViewModel
     {
+        [Display(Name = "Clave")]
+        public long clave_mem { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre(s)")]
+        public string nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Apellido pterno")]
+        public string app { get; set; }
+
+        [Required]
+        [Display(Name = "Apellido materno")]
+        public string apm { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha de nacimiento")]
+        public DateTime fecha_nac { get; set; }
+
+        [Required]
+        [Display(Name = "Colonia")]
+        public string colonia { get; set; }
+
+        [Required]
+        [Display(Name = "Calle")]
+        public string calle { get; set; }
+
+        [Required]
+        [Display(Name = "Numero")]
+        public int numero { get; set; }
+
+        [Required]
+        [Display(Name = "Tipo")]
+        public string tipo { get; set; }
+
+        [Required]
+        [Display(Name = "Puntos")]
+        public int puntos { get; set; }
     }
 
     public class GetClientsViewModel
     {
+        public int current { get; set; }
+        public int rowCount { get; set; }
+        public int total { get; set; }
+        public List<ClientsViewModel> rows;
+
+        public GetClientsViewModel()
+        {
+            rows = new List<ClientsViewModel>();
+        }
     }
 
     public class FilmFunctionsViewModel
     {
+        [Display(Name = "Clave")]
+        public long clave_fun { get; set; }
+
+        [Required]
+        [Display(Name = "Pelicula")]
+        public long clave_pel { get; set; }
+
+        [Required]
+        [Display(Name = "Sala")]
+        public long clave_sal { get; set; }
+
+        [Required]
+        [Display(Name = "Hora inicio")]
+        public DateTime hora_ini { get; set; }
+
+        [Required]
+        [Display(Name = "Hora fin")]
+        public DateTime hora_fin { get; set; }
+
+        [Required]
+        [Display(Name = "Fecha")]
+        public DateTime fecha { get; set; }
+        
+        [Display(Name = "Cupo")]
+        public int cupo { get; set; }
     }
 
     public class GetFilmFunctionsViewModel
     {
+        public int current { get; set; }
+        public int rowCount { get; set; }
+        public int total { get; set; }
+        public List<FilmFunctionsViewModel> rows;
+
+        public GetFilmFunctionsViewModel()
+        {
+            rows = new List<FilmFunctionsViewModel>();
+        }
     }
 
     public class MoviesViewModel
     {
+        [Display(Name = "Clave")]
+        public long clave_pel { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Director")]
+        public string director { get; set; }
+
+        [Required]
+        [Display(Name = "Sinopsis")]
+        public string sinopsis { get; set; }
+
+        [Required]
+        [Display(Name = "Clasificacion")]
+        public string clasificacion { get; set; }
+
+        [Required]
+        [Display(Name = "Genero")]
+        public string genero { get; set; }
     }
 
     public class GetMoviesViewModel
     {
+        public int current { get; set; }
+        public int rowCount { get; set; }
+        public int total { get; set; }
+        public List<MoviesViewModel> rows;
+
+        public GetMoviesViewModel()
+        {
+            rows = new List<MoviesViewModel>();
+        }
     }
 
     public class MovieTeathersViewModel
     {
+        [Display(Name = "Clave")]
+        public long clave_cin { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Número de salas")]
+        public int num_salas { get; set; }
+
+        [Required]
+        [Display(Name = "Colonia")]
+        public string colonia { get; set; }
+
+        [Required]
+        [Display(Name = "Calle")]
+        public string calle { get; set; }
+
+        [Required]
+        [Display(Name = "Número")]
+        public int numero { get; set; }
     }
 
     public class GetMovieTeathersViewModel
     {
+        public int current { get; set; }
+        public int rowCount { get; set; }
+        public int total { get; set; }
+        public List<MovieTeathersViewModel> rows;
+
+        public GetMovieTeathersViewModel()
+        {
+            rows = new List<MovieTeathersViewModel>();
+        }
     }
 }
